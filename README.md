2a6d0e545c71c4e2e02e6be86d08146085afb80e
Patrick <patrick.r.meadows@gmail.com>
2019-02-01 19:27:29 -0600

    Adding base files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # windowlessExample
-This is a basic example on how to create a windowless electron desktop application
+This is a basic example on how to create a frameless window electron desktop application
+
+
+To run:
+
+- Must have Node.js installed
+Check out this code
+
+npm init -y
+npm install electron -D
+
--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-const electron = require('electron')
+const {app, BrowserWindow} = require('electron')
+
+let win
+
+app.on('ready', _ => {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+    
+    win.loadFile('src/index.html')
+    win.on('closed', _ => {
+        win = null
+    })
+})
+
+app.on('window-all-closed', _ => {
+    app.quit()
+})
